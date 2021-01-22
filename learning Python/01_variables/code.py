194f5219975baddbb19df7b6c7f391d9a7216af7
Jordan Worboys <56325132+Jworboys@users.noreply.github.com>
2021-01-22 02:13:54 -0500

    Add files via upload

---
+++ learning Python/01_variables/code.py
@@ -0,0 +1,23 @@
+
+"""Integers"""
+x = 15
+price = 9.99
+discount = 0.2
+result = price * (1-discount)
+print(result)
+
+"""Strings"""
+name = "Rolf"
+print(name)
+print(name * 2)
+print(name + name)
+
+'''reassinging'''
+a = 25
+b = a
+print(a)
+print(b)
+b = 17
+print(a)
+print(b)
+
